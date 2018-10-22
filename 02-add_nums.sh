3e3fb5839eaecdc4d70590e5722b5d2aa95fb144
Cesar M. Romero <cromeropedraza@ggc.edu>
2018-10-22 19:22:38 -0400

    Git practice 2 Cesar

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -13,3 +13,13 @@ echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
+echo "Enter the width"
+read width
+echo "Enter the height"
+read height
+
+echo "Calculating number of pixels"
+#2015 macbook pro has 227 pixel per inch
+totalPixels=$((width * height* 227))
+
+echo "The number of pixels is $totalPixels"
